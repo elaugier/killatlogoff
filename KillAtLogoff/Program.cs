2a6e69ff06ee6d4e48efec42d5a1ca3971954a6f
elaugier <eric.laugier@gmail.com>
2015-02-07 20:55:40 +0100

    initial version

---
+++ KillAtLogoff/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Threading;
+using System.Diagnostics;
+
+namespace KillAtLogoff
+{
+    public sealed class Program
+    {
+
+        static void Main()
+        {
+            SystemEvents.SessionEnding += new SessionEndingEventHandler(KillProcess);
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        static void KillProcess(object sender, EventArgs e)
+        {
+            string processnames = (string)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\KILLAT\\KillAtLogoff", "ProcessNames", "");
+            string[] arrProcesses = processnames.Split(',');
+            foreach (string processname in arrProcesses)
+            {
+                Process CurrentProcess = Process.GetCurrentProcess();
+                foreach (Process proc in Process.GetProcesses())
+                {
+                    if (proc.ProcessName.Contains(processname.ToString()) && (proc.SessionId == CurrentProcess.SessionId))
+                    {
+                        proc.Kill();
+                        Console.WriteLine("Kill " + proc.ProcessName + " " + proc.Id.ToString());
+                        Thread.Sleep(3000);
+                        break;
+                    }
+                }
+            }
+            System.Environment.Exit(0);
+        }
+
+    }
+}
